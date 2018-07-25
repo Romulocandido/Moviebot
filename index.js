3e3b87b45bc346c01a01c284d933fb836330a11c
Rômulo Oliveira Candido da Silva <romulocand@gmail.com>
2018-07-25 09:01:36 -0300

    correcao no nome

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ server.post('/getMovies',function (request,response)  {
             });
     } else if(request.body.queryResult.parameters['movie-name']) {
      //   console.log('popular-movies param found');
-        let movie = request.body.result.parameters['movie-name'];
+        let movie = request.body.queryResult.parameters['movie-name'];
         var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
             req.query({
                 "include_adult": "false",
